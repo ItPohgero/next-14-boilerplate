6fe0774466dbc9722adc1ae15f34c9584bfd09a9
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-03-18 04:57:34 +0700

    feat: setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // formats: ['*'],
+        remotePatterns: [
+            {
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                hostname: 'images.lumacdn.com',
+            },
+            {
+                hostname: 'indonesiabaik.id',
+            },
+            {
+                hostname: '*',
+            },
+        ],
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(mp3|wav|m4a)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: '/_next/static/audio/',
+                    outputPath: 'static/audio/',
+                    name: '[name].[ext]',
+                },
+            },
+        })
+        return config
+    },
+}
 
-export default nextConfig;
+export default nextConfig
